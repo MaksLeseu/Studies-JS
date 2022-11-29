9c172766fd8b7bc929095dd4e43ce71cccbab825
Maks Leseu <lesov0iblr@gmail.com>
2022-11-29 13:30:56 +0100

    homeWork 1

---
+++ homeWork1/js/script.js
@@ -0,0 +1,42 @@
+
+// lesson -- 1 -------------- //
+
+var admin;
+var name = "Василий";
+
+admin = name;
+console.log(admin);
+
+
+// ------------------------------------ //
+
+var a = 10,
+    b = 3.8;
+
+var c;
+
+c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
+
+// a = i / b = e ------------------------------------ //
+
+var i = 10,
+    e = 3.8;
+
+var i = i + e;
+
+var e = i - e;
+
+var i = i - e;
+
+console.log(i);
+console.log(e);
+
+
+
+
+
