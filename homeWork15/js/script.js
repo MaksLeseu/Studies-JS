10600e48cd739527b2cd5265e282a0185349bec7
Maks Leseu <lesov0iblr@gmail.com>
2023-01-16 21:59:41 +0100

    lesson 15 task 2 - 10%

--- homeWork15/js/script.js
+++ homeWork15/js/script.js
@@ -56,3 +56,63 @@ window.onload = function() {
     localStorage.clear();
 }
 
+
+
+/* 
+  Задание 2:
+    Добавить на страницу два поля для ввода - X и Y и кнопку "Create".
+    Если хотя бы одно из полей пустое - кнопка дизэйблится (делать по keyup).
+    Поля должны принимать только целые числа от 1 до 10, в противном случае должно выводиться сообщение об ошибке
+    (делать по click).
+    По клику на кнопку должна отрисоваться шахматная доска с размерами X по горизонтали и Y по вертикали.
+    При введении значений X и Y заново таблица должна корректно перерисоваться.
+    По клику на любое поле доски - цвета всех полей должны изменяться на противоположные. 
+*/
+
+//var form = document.getElementsByClassName('form');
+var form = document.getElementById('forma');
+//var inputX = document.getElementById('input-x');
+var inputY = document.getElementById('input-y');
+var input = document.getElementsByTagName('input');
+var button = document.getElementById('button-create');
+
+
+
+
+form.addEventListener('keyup', function(event) {
+    var target = event.target;
+    
+    if (target.tagName == 'INPUT') {
+        var inp = target;
+        
+        console.log(Number(inp.value));
+
+        if (typeof Number(inp.value) === 'number' && 
+        Number(inp.value) !== NaN) {
+            console.log('number working');
+        }
+    }
+    
+});
+
+button.onclick = function() {
+    console.log('working button');
+}
+
+/*
+f (inputX.value == '') {
+    console.log('This is string');
+} else {
+    if (typeof inputX.value === 'string') {
+        console.log('Yes');
+    }
+}
+if (inputX.value == '') {
+    console.log('Yes');
+} else {
+    console.log('No')
+}
+
+*/
+
+
